d9d5cc9c42db7f5fe499318a62b0bc540dd34eb6
Lex Mel <lex07672@gmail.com>
2024-07-13 11:46:38 +0300

    feat: add routing and 404

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App';
+import NotFoundPage from './NotFoundPage';
 import './style.css';
 
 const rootElement = document.getElementById('root');
@@ -8,7 +10,12 @@ if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </BrowserRouter>
     </React.StrictMode>,
   );
 }
