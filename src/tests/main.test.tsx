b45931882d6ed9da7face6ed3aff40505503d31c
Lex Mel <lex07672@gmail.com>
2024-07-20 20:18:15 +0300

    fix: wrap updates in act() to resolve warnings

--- src/tests/main.test.tsx
+++ src/tests/main.test.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import { render } from '@testing-library/react';
+import { render, act } from '@testing-library/react';
 import { test } from 'vitest';
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
@@ -9,20 +9,24 @@ test('renders App component using createRoot', async () => {
   const root = document.createElement('div');
   document.body.appendChild(root);
 
-  const reactRoot = ReactDOM.createRoot(root);
-  reactRoot.render(
-    <React.StrictMode>
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>
-    </React.StrictMode>,
-  );
+  await act(async () => {
+    const reactRoot = ReactDOM.createRoot(root);
+    reactRoot.render(
+      <React.StrictMode>
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      </React.StrictMode>,
+    );
+  });
 });
 
 test('renders App component using testing-library', async () => {
-  render(
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>,
-  );
+  await act(async () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+  });
 });
