c7d5878d2c62d6f81b96a83c9ae1d0fdcef61e65
Lex Mel <lex07672@gmail.com>
2024-07-21 23:01:55 +0300

    fix: test, wrap app component with redux provider

--- src/tests/main.test.tsx
+++ src/tests/main.test.tsx
@@ -3,7 +3,9 @@ import { render, act } from '@testing-library/react';
 import { test } from 'vitest';
 import React from 'react';
 import { MemoryRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from '../App';
+import store from '../store/store';
 
 test('renders App component using createRoot', async () => {
   const root = document.createElement('div');
@@ -13,9 +15,11 @@ test('renders App component using createRoot', async () => {
     const reactRoot = ReactDOM.createRoot(root);
     reactRoot.render(
       <React.StrictMode>
-        <MemoryRouter>
-          <App />
-        </MemoryRouter>
+        <Provider store={store}>
+          <MemoryRouter>
+            <App />
+          </MemoryRouter>
+        </Provider>
       </React.StrictMode>,
     );
   });
@@ -24,9 +28,11 @@ test('renders App component using createRoot', async () => {
 test('renders App component using testing-library', async () => {
   await act(async () => {
     render(
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>,
+      <Provider store={store}>
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      </Provider>,
     );
   });
 });
