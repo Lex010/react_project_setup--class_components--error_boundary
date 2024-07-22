5c4940601a48d62a7b15c75e6a8dc0fe93ead45d
Lex Mel <lex07672@gmail.com>
2024-07-22 22:48:45 +0300

    feat: add theme switching functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,28 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import SearchComponent from './SearchComponent';
 import ResultsComponent from './ResultsComponent';
 import ErrorBoundary from './ErrorBoundary';
 import { RootState } from './store/store';
+import { toggleTheme } from './store/slices/themeSlice';
 import './style.css';
 
 const App: React.FC = () => {
   const searchTerm = useSelector((state: RootState) => state.search.searchTerm);
+  const theme = useSelector((state: RootState) => state.theme.theme);
+  const dispatch = useDispatch();
+
+  const handleThemeToggle = () => {
+    dispatch(toggleTheme());
+  };
 
   return (
     <ErrorBoundary>
-      <div className="app">
-        <div className="search-section">
+      <div className={`app ${theme}`}>
+        <div className={`search-section ${theme}`}>
+          <button onClick={handleThemeToggle} className="theme-toggle-btn">
+            Switch to {theme === 'light' ? 'Dark' : 'Light'} Mode
+          </button>
           <SearchComponent />
         </div>
         <div className="results-section">
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { pokemonApi } from '../services/pokemonApi';
 import searchReducer from './slices/searchSlice';
+import themeReducer from './slices/themeSlice';
+import type { ThemeState } from './slices/themeSlice';
 
 const store = configureStore({
   reducer: {
     [pokemonApi.reducerPath]: pokemonApi.reducer,
     search: searchReducer,
+    theme: themeReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(pokemonApi.middleware),
@@ -14,3 +17,4 @@ const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export default store;
+export type { ThemeState };
