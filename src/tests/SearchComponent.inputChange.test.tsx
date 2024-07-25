234aab2e58f85fb4d9b47603b2d7beeefd266d6f
Lex Mel <lex07672@gmail.com>
2024-07-25 19:25:52 +0300

    feat: searchComponent.inputChange.test.tsx

---
+++ src/tests/SearchComponent.inputChange.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../store/store';
+import SearchComponent from '../SearchComponent';
+import { test, expect } from 'vitest';
+
+test('updates input value on change', () => {
+  render(
+    <Provider store={store}>
+      <SearchComponent />
+    </Provider>,
+  );
+  const input = screen.getByRole('textbox');
+  fireEvent.change(input, { target: { value: 'new search term' } });
+  expect(input).toHaveValue('new search term');
+});
