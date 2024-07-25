6b649fd91ab490a5868b3c2a7598d20da91e10cb
Lex Mel <lex07672@gmail.com>
2024-07-25 16:36:57 +0300

    feat: add test for btn previous/next page

---
+++ src/tests/resultsComponent.test.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+import { configureStore } from '@reduxjs/toolkit';
+import ResultsComponent from '../ResultsComponent';
+import { pokemonApi } from '../services/pokemonApi';
+import searchReducer from '../store/slices/searchSlice';
+import themeReducer from '../store/slices/themeSlice';
+import pageReducer from '../store/slices/pageSlice';
+import selectedItemsReducer from '../store/slices/selectedItemsSlice';
+import { test, expect } from 'vitest';
+
+const store = configureStore({
+  reducer: {
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
+    search: searchReducer,
+    theme: themeReducer,
+    page: pageReducer,
+    selectedItems: selectedItemsReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(pokemonApi.middleware),
+});
+
+test('renders ResultsComponent and interacts with it', async () => {
+  render(
+    <Provider store={store}>
+      <MemoryRouter>
+        <ResultsComponent />
+      </MemoryRouter>
+    </Provider>,
+  );
+
+  const loadingElement = screen.queryByText(/Loading.../i);
+  if (loadingElement) {
+    await waitFor(() => {
+      expect(screen.queryByText(/Page/i)).not.toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.queryByText(/Next/i)).not.toBeInTheDocument();
+    });
+    await waitFor(() => {
+      expect(screen.queryByText(/Previous/i)).not.toBeInTheDocument();
+    });
+  } else {
+    expect(screen.getByText(/Page/i)).toBeInTheDocument();
+    fireEvent.click(screen.getByText(/Next/i));
+    expect(screen.getByText(/Page 2/i)).toBeInTheDocument();
+    fireEvent.click(screen.getByText(/Previous/i));
+    expect(screen.getByText(/Page 1/i)).toBeInTheDocument();
+  }
+});
